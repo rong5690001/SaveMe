feb1169b6ca22769c0c7acc45388e2be2bdbba4b
rong5690001 <rong5690001@gmail.com>
2017-08-10 23:04:01 +0800

    添加图案解锁

--- app/src/main/java/com/rong/map/saveme/widget/LockView.java
+++ app/src/main/java/com/rong/map/saveme/widget/LockView.java
@@ -5,10 +5,10 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.blankj.utilcode.util.LogUtils;
-import com.blankj.utilcode.util.ScreenUtils;
 import com.blankj.utilcode.util.SizeUtils;
 
 /**
@@ -18,10 +18,11 @@ import com.blankj.utilcode.util.SizeUtils;
 public class LockView extends View {
 
     Paint mPaint;
-    int screenWidth,screenHeight;
-    int[][] circelX = new int[3][3];//X坐标
-    int[][] circelY = new int[3][3];//Y坐标
-    int radius = 10;
+    int viewWidth,viewHeight;
+    int[] circelX = new int[3];//X坐标
+    int[] circelY = new int[3];//Y坐标
+    int radius = 20;
+    float strokeWidth = 2.5f;
 
     public LockView(Context context) {
         this(context, null);
@@ -33,36 +34,69 @@ public class LockView extends View {
 
     public LockView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        setMeasuredDimension(width, width);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, top + right - left);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewWidth = w;
+        viewHeight = h;
+        radius = viewWidth / 10;
     }
 
     private void init(){
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        screenWidth = ScreenUtils.getScreenWidth();
-        screenHeight = ScreenUtils.getScreenHeight();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(strokeWidth);
+//        screenWidth = ScreenUtils.getScreenWidth();
+//        screenHeight = ScreenUtils.getScreenHeight();
         int viewWidth = SizeUtils.dp2px(radius) * 9;//图案的宽度
         int viewHeight = viewWidth;//图案的高度
-
-        int startX = screenWidth - viewWidth * 2;
-        int startY = screenHeight - startX - viewHeight;
+        int sumX = 0;
+        int sumY = 0;
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if(j == 0){
-                    circelX[i][j] = startX + radius;
-                } else {
-                    circelX[i][j] += radius * 3;
-                }
-                LogUtils.d("circelX:", circelX[i][j]);
+            if(i == 0){
+                sumX = radius * 2;
+                sumY = radius * 2;
+            } else {
+                sumX += radius * 3;
+                sumY += radius * 3;
             }
+            circelX[i] = sumX;
+            circelY[i] = sumY;
+            LogUtils.d("circelX:", circelX[i]);
         }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        init();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                canvas.drawCircle(circelX[i],circelX[j], radius, mPaint);
+            }
+        }
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
 
-
-
+        }
+        return true;
     }
 }
