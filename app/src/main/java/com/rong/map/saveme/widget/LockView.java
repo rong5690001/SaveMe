3e5a33d844412b32607e046e3c61f5f6844bf296
map <map.chen@sportq.com>
2017-08-12 22:38:55 +0800

    修改图案解锁

--- app/src/main/java/com/rong/map/saveme/widget/LockView.java
+++ app/src/main/java/com/rong/map/saveme/widget/LockView.java
@@ -2,7 +2,10 @@ package com.rong.map.saveme.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -11,18 +14,33 @@ import android.view.View;
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.SizeUtils;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Administrator on 2017/8/9/009.
  */
 
 public class LockView extends View {
 
-    Paint mPaint;
-    int viewWidth,viewHeight;
-    int[] circelX = new int[3];//X坐标
-    int[] circelY = new int[3];//Y坐标
+    private static final int DEFAULT_SELECT_COLOR = Color.GREEN;
+
+    Paint mSourcePaint;
+    Paint mSelectedPaint;
+    Paint mLinePaint;
+    int viewWidth, viewHeight;
+    //    int[] circelX = new int[3];//X坐标
+//    int[] circelY = new int[3];//Y坐标
+    Point[][] points = new Point[3][3];
     int radius = 20;
     float strokeWidth = 2.5f;
+    List<Point> selectedPoints = new ArrayList<>();
+    int selectedColor = DEFAULT_SELECT_COLOR;
+    boolean isDrawing = false;//正在画图案
+    Set<Integer> selectedIndexs = new HashSet<>();
+
 
     public LockView(Context context) {
         this(context, null);
@@ -56,27 +74,43 @@ public class LockView extends View {
         radius = viewWidth / 10;
     }
 
-    private void init(){
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(strokeWidth);
+    private void init() {
+        mSourcePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mSourcePaint.setStyle(Paint.Style.STROKE);
+        mSourcePaint.setStrokeWidth(strokeWidth);
+        mSelectedPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mSelectedPaint.setColor(selectedColor);
+        mSelectedPaint.setStyle(Paint.Style.FILL);
+        mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mLinePaint.setColor(selectedColor);
+        mLinePaint.setStyle(Paint.Style.STROKE);
+        mLinePaint.setStrokeWidth(radius / 3 * 2);
 //        screenWidth = ScreenUtils.getScreenWidth();
 //        screenHeight = ScreenUtils.getScreenHeight();
         int viewWidth = SizeUtils.dp2px(radius) * 9;//图案的宽度
         int viewHeight = viewWidth;//图案的高度
         int sumX = 0;
-        int sumY = 0;
         for (int i = 0; i < 3; i++) {
-            if(i == 0){
+            int sumY = 0;
+            if (i == 0) {
                 sumX = radius * 2;
-                sumY = radius * 2;
+//                sumY = radius * 2;
             } else {
                 sumX += radius * 3;
-                sumY += radius * 3;
+//                sumY += radius * 3;
             }
-            circelX[i] = sumX;
-            circelY[i] = sumY;
-            LogUtils.d("circelX:", circelX[i]);
+            for (int j = 0; j < 3; j++) {
+                if (j == 0) {
+                    sumY = radius * 2;
+                } else {
+                    sumY += radius * 3;
+
+                }
+                points[i][j] = new Point(sumX, sumY);
+            }
+//            circelX[i] = sumX;
+//            circelY[i] = sumY;
+//            LogUtils.d("circelX:", circelX[i]);
         }
     }
 
@@ -86,17 +120,92 @@ public class LockView extends View {
         init();
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                canvas.drawCircle(circelX[i],circelX[j], radius, mPaint);
+                canvas.drawCircle(points[i][j].x, points[i][j].y, radius, mSourcePaint);
+                canvas.drawCircle(points[i][j].x, points[i][j].y, radius / 3, mSourcePaint);
             }
         }
+
+        Point prePoint = null;
+        Path path = new Path();
+
+        //解锁图案
+        for (Point selectedPoint : selectedPoints) {
+            canvas.drawCircle(selectedPoint.x, selectedPoint.y, radius, mSelectedPaint);
+            if (prePoint != null) {
+
+            }
+            path.moveTo();
+            prePoint = selectedPoint;
+        }
+
+        canvas.save();
+        canvas.translate(prePoint.x, prePoint.y);
+
+        path.lineTo(selectedPoint.x, selectedPoint.y);
+        canvas.drawPath(path, mLinePaint);
+        canvas.restore();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                Point selectPoint = getSelectPoint(event);
+                if (selectPoint != null) {
+                    selectedPoints.add(selectPoint);
+                    isDrawing = true;
+                    postInvalidate();
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                if (isDrawing) {
+                    Point selectPoint1 = getSelectPoint(event);
+                    if (selectPoint1 != null
+                            && !selectedPoints.contains(selectPoint1)) {
+                        selectedPoints.add(selectPoint1);
+                        postInvalidate();
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                removeCallbacks(actionUpRunnable);
+                postDelayed(actionUpRunnable, 100);
+                break;
 
         }
         return true;
     }
+
+    public Point getSelectPoint(MotionEvent event) {
+        int eventX = (int) event.getX();
+        int eventY = (int) event.getY();
+        int selectedIndex = 0;
+        for (Point[] point1 : points) {
+            for (Point point2 : point1) {
+                float distance = (float) Math.sqrt(Math.pow(point2.x - eventX, 2)
+                        + Math.pow(point2.y - eventY, 2));
+                if (distance <= radius) {
+                    LogUtils.d("distance:", distance);
+                    selectedIndexs.add(selectedIndex);
+                    return point2;
+                }
+                selectedIndex++;
+            }
+        }
+
+        return null;
+    }
+
+    public Runnable actionUpRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (selectedPoints != null) {
+                selectedPoints.clear();
+                selectedIndexs.clear();
+                postInvalidate();
+            }
+        }
+    };
+
 }
