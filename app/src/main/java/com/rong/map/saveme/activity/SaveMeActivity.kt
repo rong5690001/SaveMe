30077ff7a892a24279d852a398b06665481db7fa
map <map.chen@sportq.com>
2017-08-24 16:52:12 +0800

    saveme添加发送功能

--- app/src/main/java/com/rong/map/saveme/activity/SaveMeActivity.kt
+++ app/src/main/java/com/rong/map/saveme/activity/SaveMeActivity.kt
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.RotateAnimation
 import com.rong.map.saveme.R
+import com.rong.map.saveme.event.SendMsgEvent
 import kotlinx.android.synthetic.main.activity_save_me.*
+import org.greenrobot.eventbus.EventBus
 
 class SaveMeActivity : AppCompatActivity() {
 
@@ -14,6 +16,8 @@ class SaveMeActivity : AppCompatActivity() {
         sendBtn.setOnClickListener {
             var rotation = RotateAnimation(1f, 1.5f)
             sendBtn.startAnimation(rotation)
+
+            EventBus.getDefault().post(SendMsgEvent())
         }
     }
 }
--- app/src/main/java/com/rong/map/saveme/activity/SetMsgActivity.kt
+++ app/src/main/java/com/rong/map/saveme/activity/SetMsgActivity.kt
@@ -1,31 +1,18 @@
 package com.rong.map.saveme.activity
 
-import android.app.Activity
 import android.app.PendingIntent
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
-import android.telephony.SmsManager
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import com.blankj.utilcode.util.RegexUtils
-import com.blankj.utilcode.util.SPUtils
 import com.blankj.utilcode.util.ToastUtils
-import com.google.gson.Gson
 import com.rong.map.saveme.R
-import com.rong.map.saveme.SmApplication
 import com.rong.map.saveme.base.BaseActivity
 import com.rong.map.saveme.manager.SPManager
 import com.rong.map.saveme.model.MsgData
-import com.rong.map.saveme.service.LockService
-import com.rong.map.saveme.utils.CstUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class SetMsgActivity : BaseActivity(), View.OnClickListener {
@@ -80,7 +67,6 @@ class SetMsgActivity : BaseActivity(), View.OnClickListener {
         when (v.id) {
             R.id.saveBtn -> {
                 if (canSave()) {
-//                    sendSMS()
                     SPManager.putMsgData(msgData)
                     ToastUtils.showShort(getString(R.string.saved))
                     finish()
